b2d26fee7db9329275f8fb9c1ee1d571482e2321
unknown <aprabaswara@gmail.com>
2021-10-12 20:54:09 +0100

    WG21P2.R

---
+++ WG21P2.R
@@ -0,0 +1,9 @@
+##Group member:
+##1. Aditya Prabaswara Mardjikoen(s2264710)
+##2. Xiangtian Duan(s2248742)
+##3. Huiying Chen(s2264943)
+
+n <- 5500000
+beta <- rlnorm(n,0,0.5)
+beta <- beta/mean(beta)
+lambda <- 0.4/n
