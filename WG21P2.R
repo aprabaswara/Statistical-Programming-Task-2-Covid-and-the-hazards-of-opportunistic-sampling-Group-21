b173a4384a7bfc6c70083b9ba3662c77ea6a76ea
2248742 <91072881+2248742@users.noreply.github.com>
2021-10-19 23:09:14 +0800

    WG21P2.R

--- WG21P2.R
+++ WG21P2.R
@@ -719,9 +719,115 @@ nt<-150
 beta<- rlnorm(n,0,0.5); beta <- beta/mean(beta)
 covid_simulation(n,nt,beta)
 ######end check the running time
-par(mfcol=c(5,2),mar=c(4,4,1,1))
+par(mfcol=c(4,3),mar=c(4,4,1,1))
 for (i in 1:10){
   beta<- rlnorm(n,0,0.5); beta <- beta/mean(beta)
   covid_simulation(n,nt,beta) 
 }
 
+#correction about newly infected
+covid_simulation<- function(n,nt,beta){
+  
+  ne=10
+  gamma=1/3
+  delta=1/5
+  lamda=0.4/n
+  x<-rep(0,n)
+  
+  
+  
+  
+  
+  beta_sort<-sort(beta,decreasing = TRUE)
+  beta_lower <- tail(beta_sort,n*0.1)
+  lower_index<-which(beta%in%beta_lower)
+  
+  
+  sample_beta<-sample(beta,n*0.001)
+  sample_index<-which(beta%in%sample_beta)
+  
+  
+  x[1:ne]<-1 #exposed
+  y <- x[lower_index]
+  z <- x[sample_index]
+  
+  #S<-E<-I<-R<-rep(0,nt);A<-W<-U<-P<-rep(0,nt); D<-Q<-G<-K<-rep(0,nt)
+  I<-U<-G<-rep(0,nt)
+  
+  # S[1]<-n-ne;E[1]<-ne;A[1]<-n-ne;W[1]<-ne;D[1]<-n-ne;Q[1]<-ne
+  
+  
+  
+  for (i in 2:nt){
+    
+    u<-runif(n)
+    
+    sum_x<-sum(x==0)
+    sum_y<-sum(y==0)
+    sum_z<-sum(z==0)
+    
+    sum_beta_I<-sum(beta[which(x==2)])
+    
+    x[x==2&u<delta]<-3## I -> R with prob delta
+    
+    x[x==1&u<gamma]<-2 ## E -> I with prob gamma
+    
+    
+    
+    x[which(x==0)][u[which(x==0)]<sum_beta_I*lamda*beta[which(x==0)]]<-1
+    y <- x[lower_index]
+    z <- x[sample_index]
+    
+    
+    newly_infected_x<-sum_x-sum(x==0)
+    I[i]<-newly_infected_x
+    
+    newly_infected_y<-sum_y-sum(y==0)
+    U[i]<-newly_infected_y
+    
+    newly_infected_z<-sum_z-sum(z==0)
+    G[i]<-newly_infected_z
+    
+    
+    
+    #x[which(x==0)][u[which(x==0)]<sum(beta[which(x==2)])*lamda*beta[which(x==0)]]<-1
+    
+    #S[i]<-sum(x==0);E[i]<-sum(x==1);
+    #I[i]<-sum(x==2)-sum_x
+    #;R[i]<-sum(x==3)
+    
+    #A[i]<-sum(y==0);W[i]<-sum(y==1);
+    #U[i]<-sum(y==2)-sum_y
+    #;P[i]<-sum(y==3)
+    
+    #D[i]<-sum(z==0);Q[i]<-sum(z==1);
+    # G[i]<-sum(z==2)-sum_z
+    #;K[i]<-sum(z==3)
+    
+    
+    
+    
+  }
+  
+  
+  #list (S=S,E=E,I=I,R=R,beta=beta);list (A=A,W=W,U=U,P=P,beta=beta);list (D=D,Q=Q,G=G,K=K,beta=beta)
+  list (I=I,U=U,G=G,beta=beta)
+  
+  plot(I/n,ylim=c(0,max(I/n,U/(0.1*n),G/(0.001*n))),xlab="day",ylab="N",col='red',type='l')
+  points(U/(0.1*n),ylim=c(0,max(U/(0.1*n))),xlab="day",ylab="N",col='green',type='l')
+  lines(G/(0.001*n),ylim=c(0,max(G/(0.001*n))),xlab="day",ylab="N",col='blue',type='l')
+  
+  ###plot(I,ylim=c(0,max(I)),xlab="day",ylab="N",col='red',type='l')
+  ###points(U,ylim=c(0,max(U)),xlab="day",ylab="N",col='green',type='l')
+  ###lines(G,ylim=c(0,max(G)),xlab="day",ylab="N",col='blue',type='l')
+}
+n<-5500000
+nt<-150
+beta<- rlnorm(n,0,0.5); beta <- beta/mean(beta)
+covid_simulation(n,nt,beta)
+
+par(mfcol=c(4,3),mar=c(4,4,1,1))
+for (i in 1:10){
+  beta<- rlnorm(n,0,0.5); beta <- beta/mean(beta)
+  covid_simulation(n,nt,beta) 
+}
